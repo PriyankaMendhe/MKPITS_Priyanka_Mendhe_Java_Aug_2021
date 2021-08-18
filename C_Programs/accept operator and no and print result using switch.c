5c68c87fecb3d821c14f5f7c969966723cc0e427
Priyanka Mendhe <priyankamd12@gmail.com>
2021-08-18 18:49:22 +0530

    adding switch statment program

---
+++ C_Programs/accept operator and no and print result using switch.c
@@ -0,0 +1,13 @@
+ //4 -wap to print no. from 10 to 20 using while loop.
+ #include<stdio.h>
+ #include<conio.h>
+ int main()
+ {
+ 	int i=10;
+ 	while(i<=20)
+ 	{
+ 		printf("\n%d",i);
+ 		i++;
+	 }
+	 return 0;
+ }
