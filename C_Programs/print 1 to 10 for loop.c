1be59133d409402249d5f9e6dfcb7e277f906bb7
Priyanka Mendhe <priyankamd12@gmail.com>
2021-08-19 22:59:36 +0530

    adding pgm of for loop

---
+++ C_Programs/print 1 to 10 for loop.c
@@ -0,0 +1,12 @@
+//2- wap to print no. from 1 to 10 using for loop.
+#include<stdio.h>
+int main()
+{
+	int i;
+	for(i=10;i>=1;i--)
+	{
+		printf("\n %d",i);
+		
+	}
+	return 0;
+}
---
+++ C_Programs/print 20 to 30 using for loop.c
@@ -0,0 +1,12 @@
+//3 -wap to print no. from 20 to 30 using for loop.
+#include<stdio.h>
+int main()
+{
+	int i;
+	for(i=20;i<=30;i++)
+	{
+		printf("\n %d",i);
+		
+	}
+	return 0;
+}
