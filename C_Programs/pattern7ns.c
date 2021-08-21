ff8cf6bd1044fc255b838c2f596d3b68bf383719
Priyanka Mendhe <priyankamd12@gmail.com>
2021-08-21 18:51:50 +0530

    Adding Pattern pgm using for loop

---
+++ C_Programs/pattern7ns.c
@@ -0,0 +1,19 @@
+//7 - assignment 
+//wap    
+//		* 	*	*	*	
+//		*	*	*
+//		*	*
+//		*
+int main()
+{
+	 int row,col;
+	for(row=4;row>=1;row++)
+	{
+		for(col=1;col<=row;col++)
+		{
+			printf("\t *");
+		}
+		
+		printf("\n");
+	}
+}
