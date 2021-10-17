be9f3453a127fbe577046dbe7d6b85c78bd46889
Priyanka Mendhe <priyankamd12@gmail.com>
2021-10-18 02:03:55 +0530

    Multiple Catch Block pgm

---
+++ Java_Programs/MultipleCatchBlock4.java
@@ -0,0 +1,29 @@
+    public class MultipleCatchBlock4{
+
+        public static void main(String[] args) {
+
+               try{
+
+
+						int a[]=new int[5];
+						a[5]=30/0;
+						System.out.println(a[10]);
+
+                   }
+                   catch(ArithmeticException e)
+                      {
+                       System.out.println("Arithmetic Exception occurs");
+                        int a1[]=new int[5];
+                        try
+                         {
+								  a1[5]=30;
+					     }
+				        catch(ArrayIndexOutOfBoundsException e1)
+					   {
+						 System.out.println("Array index Exception occurs");
+                       }
+
+                   System.out.println("rest of the code");
+        }
+    }
+}
---
+++ Java_Programs/MultipleCatchBlock6.java
@@ -0,0 +1,14 @@
+class MultipleCatchBlock6{
+      public static void main(String args[]){
+       try{
+        int a[]=new int[5];
+        a[5]=30/0;
+       }
+      // catch(Exception e){System.out.println("common task completed");}  // error if parent class order is 1st
+       catch(ArithmeticException e){System.out.println("task1 is completed");}
+       catch(ArrayIndexOutOfBoundsException e){System.out.println("task 2 completed");}
+       catch(Exception e){System.out.println("common task completed");}
+       System.out.println("rest of the code...");
+     }
+    }
+
