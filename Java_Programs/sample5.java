9ed054566e01ec8615283c74cb4d276cc9c6cb94
Priyanka Mendhe <priyankamd12@gmail.com>
2021-10-18 01:39:28 +0530

    Exception Handling pgm

---
+++ Java_Programs/sample5.java
@@ -0,0 +1,23 @@
+
+//Exception handling program
+class sample5 {
+	public static void main(String[] arg) {
+		int arr[] =new int[5];
+		        int res=0;
+		        try {
+			String s="abc";
+    int i=Integer.parseInt(s);//NumberFormatException
+
+		        }catch (Exception ee) {
+		            System.out.println(ee.toString());
+
+		        }
+		        finally {
+		            System.out.println("finally executed");
+		        }
+		        System.out.println("bye");
+
+		System.out.println("program reached successfully till end");
+		}
+
+}
