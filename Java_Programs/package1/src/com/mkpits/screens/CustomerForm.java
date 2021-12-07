94b6e3fe80085d6fa876e1dd3ff64d35e664de3a
Priyanka Mendhe <priyankamd12@gmail.com>
2021-12-08 01:11:25 +0530

    Customer Package

---
+++ Java_Programs/package1/src/com/mkpits/screens/CustomerForm.java
@@ -0,0 +1,11 @@
+package com.mkpits.screens;
+import customers.customer;
+public class CustomerForm {
+
+    public static void main(String args[])
+    {
+        customer c = new customer();
+        c.display();
+
+    }
+}
---
+++ Java_Programs/package1/src/customers/customer.java
@@ -0,0 +1,11 @@
+package customers;
+
+public class customer {
+    
+    public void display()
+    {
+
+        System.out.println("Hello");
+    }
+
+}
